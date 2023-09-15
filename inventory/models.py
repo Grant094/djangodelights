90f33b800f916e6f971a89468a47bfeb44502c22
Grant Oxer <grant.oxer@gmail.com>
2023-09-15 16:51:03 -0700

    created Ingredient model

--- inventory/models.py
+++ inventory/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+class Ingredient(models.Model):
+    name = models.CharField()
+    quantity = models.FloatField(default=0.0)
+    unit = models.CharField()
+    unit_price = models.FloatField(default=0.0)
