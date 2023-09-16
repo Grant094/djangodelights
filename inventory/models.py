2bf82802675be89acc9d93bf707aabbf30ee4ea7
Grant Oxer <grant.oxer@gmail.com>
2023-09-15 17:48:00 -0700

    created RecipeRequirement model

--- inventory/models.py
+++ inventory/models.py
@@ -10,3 +10,8 @@ class Ingredient(models.Model):
 class MenuItem(models.Model):
     title = models.CharField()
     price = models.FloatField(default=0.0)
+
+class RecipeRequirement(models.Model):
+    menu_item = models.ForeignKey(MenuItem, on_delete=models.CASCADE)
+    ingredient = models.ForeignKey(Ingredient, on_delete=models.CASCADE)
+    quantity = models.FloatField(default=0.0)
