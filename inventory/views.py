25ea7e1452b6818a490c9407b0cd3e0aca0d33d0
Grant Oxer <grant.oxer@gmail.com>
2023-09-18 11:17:22 -0700

    added ListViews for Ingredient, MenuItem and Purchase models and added DeleteView for Ingredient model

--- inventory/views.py
+++ inventory/views.py
@@ -1,3 +1,21 @@
 from django.shortcuts import render
+from .models import *
+from django.views.generic import ListView
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
 
 # Create your views here.
+class IngredientList(ListView):
+    model = Ingredient
+    template_name = "inventory/ingredient_list.html"
+
+class IngredientDelete(DeleteView):
+    model = Ingredient
+    template_name = "inventory/ingredient_delete_form.html"
+
+class MenuItemList(ListView):
+    model = MenuItem
+    template_name = "inventory/menuitem_list.html"
+
+class PurchaseList(ListView):
+    model = Purchase
+    template_name = "inventory/purchase_list.html"
