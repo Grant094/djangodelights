aecc46d078b738fc684da8afcff015e400c940c8
Grant Oxer <grant.oxer@gmail.com>
2023-09-20 11:21:14 -0700

    Created ReportView and made db changes

--- inventory/views.py
+++ inventory/views.py
@@ -1,7 +1,9 @@
 from django.shortcuts import render
 from .models import *
-from django.views.generic import ListView
+from django.views.generic import TemplateView, ListView
 from django.views.generic.edit import CreateView, UpdateView, DeleteView
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.contrib.auth import logout
 
 # Create your views here.
 class IngredientList(ListView):
@@ -19,3 +21,22 @@ class MenuItemList(ListView):
 class PurchaseList(ListView):
     model = Purchase
     template_name = "inventory/purchase_list.html"
+
+class ReportView(TemplateView):
+    template_name = "inventory/reports.html"
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context["purchases"] = Purchase.objects.all()
+        revenue = 0
+        total_cost = 0
+        for purchase in Purchase.objects.all():
+            revenue += purchase.menu_item.price
+            for recipe_requirement in purchase.menu_item.reciperequirement_set.all():
+                total_cost += recipe_requirement.ingredient.unit_price * recipe_requirement.quantity
+        
+        context["revenue"] = revenue
+        context["total_cost"] = total_cost
+        context["profit"] = revenue - total_cost
+
+        return context
